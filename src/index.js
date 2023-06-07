f52c89b61f0dc0732b18b344eb76ba34c6749410
BuonicosII <118397101+BuonicosII@users.noreply.github.com>
2023-06-07 21:18:34 +0200

    Create a module for the To Do class
    Create a module for the To Do class and import it into index.js for use

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
 import './style.css';
+import { ToDo } from "./todo";
+
+let prova = new ToDo ("enrico", "enrico", "enrico", "enrico");
+
+console.log(prova.title)
---
+++ src/todo.js
@@ -0,0 +1,26 @@
+class ToDo {
+    constructor (title, description, dueDate, priority) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+    }
+
+    set setTitle(value) {
+        this.title = value;
+    }
+
+    set setDescription(value) {
+        this.description = value;
+    }
+
+    set setDueDate(value) {
+        this.dueDate = value;
+    }
+
+    set setPriority(value) {
+        this.priority = value;
+    }
+}
+
+export { ToDo }
